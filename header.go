c2d643691656537b4b94f6c7391bcb5486556e7b
Sheena Artrip <sheena.artrip@gmail.com>
2016-03-03 16:50:00 -0500

    ID should be omitempty

--- header.go
+++ header.go
@@ -2,7 +2,7 @@ package xco
 
 // Header contains the common fields for every XEP-0114 message
 type Header struct {
-	ID   string `xml:"id,attr"`
+	ID   string `xml:"id,attr,omitempty"`
 	From string `xml:"from,attr"` //TODO: make address type
 	To   string `xml:"to,attr"`   //TODO: make address type
 }
