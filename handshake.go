e0d7de267f70399f8e9234a679360fc6c1c7a363
Sheena Artrip <sheena.artrip@gmail.com>
2017-04-30 13:22:57 -0400

    Refactor handshake
     - Split out handshake process
     - Fix bug: no longer continue on empty ID attributes

--- handshake.go
+++ handshake.go
@@ -4,17 +4,51 @@ import (
 	"crypto/sha1"
 	"encoding/hex"
 	"encoding/xml"
-	"errors"
 	"fmt"
+	"io"
+
+	"github.com/pkg/errors"
 )
 
-func (c *Component) handshakeState() (st stateFn, err error) {
-
-	if _, err = c.conn.Write([]byte(fmt.Sprintf(`
+func sendStreamStart(w io.Writer, name string) (err error) {
+	_, err = w.Write([]byte(fmt.Sprintf(`
 		<stream:stream
 			xmlns='jabber:component:accept'
 			xmlns:stream='http://etherx.jabber.org/streams'
-			to='%s'>`, c.name))); err != nil {
+			to='%s'>`, name)))
+	return
+}
+
+func sendHandshake(w io.Writer, id string, sharedSecret string) (err error) {
+	handshakeInput := id + sharedSecret
+	handshake := sha1.Sum([]byte(handshakeInput))
+	hexHandshake := hex.EncodeToString(handshake[:])
+	if _, err = w.Write([]byte(fmt.Sprintf("<handshake>%s</handshake>", hexHandshake))); err != nil {
+		return
+	}
+	return
+}
+
+func findStreamID(stream *xml.StartElement) (id string, err error) {
+
+	for _, a := range stream.Attr {
+		if a.Name.Local == "id" {
+			id = a.Value
+		}
+	}
+
+	if id == "" {
+		err = errors.New("Unable to find ID in stream response")
+		return
+	}
+
+	return
+}
+
+func (c *Component) handshakeState() (st stateFn, err error) {
+
+	if err = sendStreamStart(c, c.name); err != nil {
+		err = errors.Wrapf(err, "Error sending streamStart")
 		return
 	}
 
@@ -30,28 +64,21 @@ func (c *Component) handshakeState() (st stateFn, err error) {
 		}
 
 		var id string
-
-		for _, a := range stream.Attr {
-			if a.Name.Local == "id" {
-				id = a.Value
-			}
+		id, err = findStreamID(&stream)
+		if err != nil {
+			err = errors.Wrapf(err, "Failed to find ID attribute in stream response")
+			return
 		}
 
-		if id == "" {
-			err = errors.New("Unable to find ID in stream response")
-		}
-
-		handshakeInput := id + c.sharedSecret
-		handshake := sha1.Sum([]byte(handshakeInput))
-		hexHandshake := hex.EncodeToString(handshake[:])
-		if _, err = c.conn.Write([]byte(fmt.Sprintf("<handshake>%s</handshake>", hexHandshake))); err != nil {
+		if err = sendHandshake(c, id, c.sharedSecret); err != nil {
+			err = errors.Wrapf(err, "Failed to send handshake")
 			return
 		}
 
 		//TODO: separate each step into a state
 
-		// get handshake response
-		t, err = c.dec.Token()
+		// get handshake response, but ignore it
+		_, err = c.dec.Token()
 		if err != nil {
 			return
 		}
